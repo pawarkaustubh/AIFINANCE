853867139361971f527afc524f7882ae591f4d27
Kaustubh Pawar <pawarkaustubh2906@gmail.com>
2025-08-21 18:09:23 +0530

    integrated clerk auth

---
+++ components/Header.jsx
@@ -0,0 +1,34 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import { User } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { Button } from './ui/button'
+
+const Header = () => {
+  return (
+    <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src={"/logo.png"}
+            alt="Welth Logo"
+            width={200}
+            height={60}
+            className="h-12 w-auto object-contain"
+          />
+        </Link>
+      <SignedOut>
+            <SignInButton forceRedirectUrl='/dashboard'>
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton/>
+      </SignedIn>
+     </nav>
+    </div>
+  )
+}
+
+export default Header
