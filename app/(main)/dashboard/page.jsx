1142fb744f3279532b9340b65e5e557fae59d99f
Kaustubh Pawar <pawarkaustubh2906@gmail.com>
2025-08-22 22:23:23 +0530

    created models

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CreateAccountDrawer } from '@/components/create-account-drawer'
+
+const DashboardPage = () => {
+  return (
+    <div className='px-5'>
+      <CreateAccountDrawer>Open</CreateAccountDrawer>
+    </div>
+  )
+}
+
+export default DashboardPage
