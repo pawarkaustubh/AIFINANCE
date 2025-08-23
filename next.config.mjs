be19f1104e422184d5938cb8f9337f14ad507cf7
Kaustubh Pawar <pawarkaustubh2906@gmail.com>
2025-08-24 04:50:22 +0530

    modified next.config

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "randomuser.me",
-        pathname: "/api/portraits/**", // optional, narrows allowed paths
       },
     ],
   },
